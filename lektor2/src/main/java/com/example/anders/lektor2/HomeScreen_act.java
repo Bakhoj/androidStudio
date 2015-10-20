694189e03146107c21ddc82b77fdcde121db6284
bakhoj <anders2108@gmail.com>
2015-10-20 22:22:56 +0200

    Laptop: prøver NFC

--- lektor2/src/main/java/com/example/anders/lektor2/HomeScreen_act.java
+++ lektor2/src/main/java/com/example/anders/lektor2/HomeScreen_act.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.nfc.NfcAdapter;
 
 public class HomeScreen_act extends Activity implements View.OnClickListener{
 
